6cfa71e91e7919af05f8fff86630c6f66a7c54e7
Mugunthan93 <kskarthick93@gmail.com>
2019-07-27 17:27:24 +0530

    dragula init

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-var app = angular.module('dragula', [angularDragula(angular)]);
+var app = angular.module('angular-dragula-demo', [angularDragula(angular)]);
 
-app.controller('dragulaController', ['$scope', function dragulaController($scope) {
 
-    $scope.$on('second-bag.drag', function (e, el) {
-        el.removeClass('ex-moved');
-    });
+app.controller('MainCtrl', function ($scope, dragulaService) {
 
-    $scope.$on('second-bag.drop', function (e, el) {
-        el.addClass('ex-moved');
-    });
-
-    $scope.$on('second-bag.over', function (e, el, container) {
-        container.addClass('ex-over');
-    });
-
-    $scope.$on('second-bag.out', function (e, el, container) {
-        container.removeClass('ex-over');
-    });
-
-}]);
+    console.log(dragulaService);
+});
