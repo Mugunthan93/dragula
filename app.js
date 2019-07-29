83eb4cf03fa59926d0d8dff7c845a8c1f7889971
Mugunthan93 <kskarthick93@gmail.com>
2019-07-29 17:52:07 +0530

    direction:horizontal

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ var app = angular.module('angular-dragula-demo', [angularDragula(angular)]);
 app.controller('MainCtrl', function ($scope, dragulaService) {
 
     console.log(dragulaService);
+
+    dragulaService.options($scope, 'bag-one', {
+        direction: function () {
+            return 'horizontal';
+        }
+    })
+
+    $scope.items = [
+        "one", "two", "three"
+    ];
+
 });
